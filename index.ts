17addf4fc2d48f19e4bdf7537f623b75ce936926
Raul Macarie <raul@macarie.me>
2020-07-11 01:48:35 +0200

    Add documentation

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
+/**
+ * Lazily splits into groups the length of `size` an array. If `array` can't be split evenly, the final chunk will be the remaining elements.
+ *
+ * @param array The array to process.
+ * @param size The length of each chunk.
+ *
+ * @yields A chunk of `array` containing `size` elements. With each iteration the slice range increseas by `size`, starting with `[0, size)`, it continues to `[size, size + size)`, and so on.
+ *
+ * @example
+ * ```ts
+ * import { trancio } from 'trancio'
+ *
+ * const array = [...Array(5).keys()]
+ * const chunks = [...trancio(array, 3)]
+ *
+ * console.log(chunks)
+ * // => [ [0, 1, 2], [3, 4] ]
+ * ```
+ */
 export function* trancio<T>(
 	array: T[],
 	size: number
